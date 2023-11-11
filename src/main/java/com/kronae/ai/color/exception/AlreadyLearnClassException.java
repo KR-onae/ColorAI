e3c76936b950a194e4ae464f21295bcac639651c
KRonae <112303878+KR-onae@users.noreply.github.com>
2023-11-11 20:38:47 +0900

    Delete src directory

--- src/main/java/com/kronae/ai/color/exception/AlreadyLearnClassException.java
+++
@@ -1,7 +0,0 @@
-package com.kronae.ai.color.exception;
-
-public class AlreadyLearnClassException extends RuntimeException {
-    public AlreadyLearnClassException(String s) {
-        super(s);
-    }
-}
--- src/main/java/com/kronae/ai/color/exception/AlreadyLearnColorException.java
+++
@@ -1,7 +0,0 @@
-package com.kronae.ai.color.exception;
-
-public class AlreadyLearnColorException extends RuntimeException {
-    public AlreadyLearnColorException(String s) {
-        super(s);
-    }
-}
